2361cffab24219ef725d002c7da5a2d8e77c8cf5
masa300 <66722784+masa300@users.noreply.github.com>
2020-12-27 15:36:36 +0900

    fxml実装失敗

--- src/main/java/jp/masa/screensamplemod/ScreenFX.java
+++ src/main/java/jp/masa/screensamplemod/ScreenFX.java
@@ -1,14 +1,23 @@
 package jp.masa.screensamplemod;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.ResourceLocation;
+
+import java.io.InputStream;
 
 public class ScreenFX extends Application {
     @Override
-    public void start(Stage stage) {
-        stage.setTitle("JavaFX Screen");
-        stage.setWidth(500);
-        stage.setHeight(500);
+    public void start(Stage stage) throws Exception {
+        System.out.println(Minecraft.getMinecraft().getResourceManager().getResourceDomains());
+        stage.setTitle("RTM Train Monitor");
+        InputStream is = Minecraft.getMinecraft().getResourceManager().getResource(new ResourceLocation(ScreenSampleMod.MODID, "default.fxml")).getInputStream();
+        Parent root = new FXMLLoader().load(is);
+        stage.setScene(new Scene(root));
         stage.show();
     }
 
--- src/main/java/jp/masa/screensamplemod/ScreenSampleMod.java
+++ src/main/java/jp/masa/screensamplemod/ScreenSampleMod.java
@@ -1,18 +1,16 @@
 package jp.masa.screensamplemod;
 
-import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 
 @Mod(modid = ScreenSampleMod.MODID, name = ScreenSampleMod.MODNAME, version = ScreenSampleMod.VERSION)
 public class ScreenSampleMod {
-    public static final String MODID = "ScreenSampleMod";
-    public static final String MODNAME = "Screen Sample Mod";
+    public static final String MODID = "rtmtrainmonitor";
+    public static final String MODNAME = "RTM Train Monitor";
     public static final String VERSION = "1.0.0";
 
     @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {
+    public void init(FMLPostInitializationEvent event) {
         new Thread(ScreenFX::run).start();
-        FMLCommonHandler.instance().bus().register(new ScreenSwing());
     }
 }
